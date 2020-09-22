587eab918020797414bdf48e64fd010a6060343b
Iso <kagiso.k.mashigo@gmial.com>
2020-09-22 17:46:41 +0000

    animation function added and working

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+
+const animation = function(str) {
+let timer = 0
+  for (const char of str) {
+    setTimeout(() => { process.stdout.write(char); }, timer) // <= 1s delay to make it noticeable. Can dial it down later.
+    timer += 50;
+  }
+  setTimeout(() => {process.stdout.write("\n")}, timer);
+};
+
+animation("hello there from lighthouse labs");
+
