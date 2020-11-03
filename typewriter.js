10bf40f445fe5f87e9897b11c2fc44030f0f26ff
Iso <kagiso.k.mashigo@gmial.com>
2020-11-03 13:52:19 +0000

    file updated

--- typewriter.js
+++ typewriter.js
@@ -2,11 +2,11 @@
 const animation = function(str) {
 let timer = 0
   for (const char of str) {
-    setTimeout(() => { process.stdout.write(char); }, timer) // <= 1s delay to make it noticeable. Can dial it down later.
+    setTimeout(() => {process.stdout.write(char)}, timer); // <= 1s delay to make it noticeable. Can dial it down later.
     timer += 50;
   }
   setTimeout(() => {process.stdout.write("\n")}, timer);
 };
 
-animation("hello there from lighthouse labs");
+animation("hello there from lighthouse labs, c'est moi");
 
